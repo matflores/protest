3a4d8a11b8b3581f65f304487d7bbd3aea64e3c7
Matías Flores <mflores@atlanware.com>
2010-03-23 14:19:25 -0300

    Allow mock test cases to be run with a report other than :progress

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,15 +24,16 @@ module TestHelpers
     Protest.report(type, IORecorder.new)
   end
 
-  def mock_test_case(&block)
+  def mock_test_case(report=:progress, &block)
     test_case = Protest.describe(name, &block)
     test_case.description = ""
     nested_contexts = Protest.send(:available_test_cases).select {|t| t < test_case }
 
-    report = silent_report
-    [test_case, *nested_contexts].each do |test_case|
-      test_case.run(Protest::Runner.new(report))
-    end
+    report = silent_report(report)
+    Protest::Runner.new(report).run(*[test_case, *nested_contexts])
+#    [test_case, *nested_contexts].each do |test_case|
+#      test_case.run(Protest::Runner.new(report))
+#    end
     report
   end
 end
