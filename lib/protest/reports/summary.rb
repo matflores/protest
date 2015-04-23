c3a21a482d565478c6a08d573bdc85885e6ac19a
Matías Flores <flores.matias@gmail.com>
2015-04-23 10:58:57 -0300

    Summary report should list all failures and errors

--- lib/protest/reports/summary.rb
+++ lib/protest/reports/summary.rb
@@ -2,8 +2,12 @@ module Protest
   # The +:summary+ report will output a brief summary with the total number
   # of tests, assertions, passed tests, pending tests, failed tests and
   # errors.
+  #
+  # It will also output the list of pending tests, failed tests and errors.
   class Reports::Summary < Report
     on :end do |report|
+      report.summarize_pending_tests
+      report.summarize_errors
       report.summarize_test_totals
     end
   end
